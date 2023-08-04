087b07286142f0209cbb538a989399759b19c8cf
FUJISE Takumi <106900036+tkmfujise@users.noreply.github.com>
2023-08-05 04:23:07 +0900

    リバーシがひっくり返せるところまで

--- Reversi3D/src/Game/Game.gd
+++ Reversi3D/src/Game/Game.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+signal turn_changed
+
 const MAX_LOCATION_NUMBER = 8
 @onready var DiskScene = preload("res://src/Disk/Disk.tscn")
 var current_color : Disk.COLOR
@@ -18,23 +20,45 @@ func initialize() -> void:
     current_color = Disk.COLOR.WHITE
 
 
-func put(location: Vector2i, color: Disk.COLOR) -> void:
+func place(location: Vector2i) -> void:
+    if invalid_place(location, current_color):
+        printerr("invalid place tried: ", location)
+    else:
+        var disk = put(location, current_color)
+        try_reverse_by(disk)
+    take_turn()
+
+
+func put(location: Vector2i, color: Disk.COLOR) -> Disk:
     var disk = DiskScene.instantiate()
     disk.location = location
     disk.color    = color
     $Board.add_disk(disk)
+    return disk
 
 
 func clear_disks() -> void:
     $Board.clear_disks()
 
 
-func get_disks() -> Array[Disk]:
+func get_disks() -> Array[Node]:
     return $Board.get_disks()
 
 
+func get_disk(location: Vector2i) -> Disk:
+    var arr = get_disks().filter(
+        func(d): return d.location == location)
+    return arr[0] if arr else null
+
+
 func invalid_place(location: Vector2i, color: Disk.COLOR) -> bool:
-    return out_of_range(location)
+    return already_placed(location) \
+        or out_of_range(location) \
+        or reversible_count(location, color) == 0
+
+
+func already_placed(location: Vector2i) -> bool:
+    return !!get_disk(location)
 
 
 func out_of_range(location: Vector2i) -> bool:
@@ -44,6 +68,73 @@ func out_of_range(location: Vector2i) -> bool:
     )
 
 
+func try_reverse_by(disk: Disk) -> void:
+    for other in reversible_disks_by(disk):
+        other.reverse()
+
+
+func reversible_count(location: Vector2i, color: Disk.COLOR) -> int:
+    var disk = DiskScene.instantiate()
+    disk.location = location
+    disk.color    = color
+    return reversible_disks_by(disk).size()
+
+
+func reversible_disks_by(disk: Disk) -> Array:
+    var reversible_disks = []
+    for locations in directional_locations_from(disk.location):
+        var line = []
+        for next_location in locations:
+            var next = get_disk(next_location)
+            if next && next.color != disk.color:
+                line.append(next)
+            elif line && next && next.color == disk.color:
+                line.append(next)
+                break
+            else: line = []; break
+        if line.size() > 1 && line.pop_back().color == disk.color:
+            reversible_disks = reversible_disks + line
+    return reversible_disks
+
+
+# (5,5) => [
+#   [(5,4), (5,3), (5,2), (5,1)],  # ↑
+#   [(6,4), (7,3), (8,2)],         # ➚
+#   [(4,4), (3,3), (2,2), (1,1)],  # ↖
+#   [(6,5), (7,5), (8,5)],         # →
+#   [(4,5), (3,5), (2,5), (1,5)],  # ←
+#   [(6,6), (7,7), (8,8)],         # ➘
+#   [(4,6), (3,7), (2,8)],         # ↙
+#   [(5,6), (5,7), (5,8)],         # ↓
+# ]
+func directional_locations_from(base: Vector2i) -> Array[Array]:
+    return [
+        locations_line_from(base,  0, -1), # ↑
+        locations_line_from(base,  1, -1), # ➚
+        locations_line_from(base, -1, -1), # ↖
+        locations_line_from(base,  1,  0), # →
+        locations_line_from(base, -1,  0), # ←
+        locations_line_from(base,  1,  1), # ➘
+        locations_line_from(base, -1,  1), # ↙
+        locations_line_from(base,  0,  1), # ↓
+    ]
+
+
+# locations_line_from(Vector2i(5, 5), -1, 1) => [(4, 6), (3, 7)...]
+func locations_line_from(base: Vector2i, dx: int, dy: int) -> Array:
+    var arr = []
+    for i in range(1, MAX_LOCATION_NUMBER + 1):
+        arr.append(Vector2i(base.x + (dx * i), base.y + (dy * i)))
+    return arr.filter(func(v): return !out_of_range(v))
+
+
+func take_turn() -> void:
+    if current_color == Disk.COLOR.WHITE:
+        current_color = Disk.COLOR.BLACK
+    else:
+        current_color = Disk.COLOR.WHITE
+    emit_signal("turn_changed")
+
+
 func _on_board_clicked(location: Vector2i) -> void:
-    if !invalid_place(location, current_color):
-        put(location, current_color)
+    place(location)
--- Reversi3D/src/disk/Disk.gd
+++ Reversi3D/src/disk/Disk.gd
@@ -3,12 +3,9 @@ extends Node3D
 class_name Disk
 
 enum COLOR { WHITE, BLACK }
-const COLOR_ROTATION = {
-    'white': Vector3(0, 0, 0),
-    'black': Vector3(0, 0, -180),
-}
 
 @onready var mesh = $Mesh
+@onready var animation = $Mesh/AnimationPlayer
 @export var color : COLOR : set = change_color
 @export var location : Vector2i
 
@@ -23,9 +20,20 @@ func change_color(_color: COLOR) -> void:
 
 
 func set_mesh_rotation() -> void:
-    if !mesh: return
+    if !animation: return
     if color == COLOR.WHITE:
-        mesh.rotation_degrees = Vector3(0, 0, 0)
+        animation.set("current_animation", "WhiteToBlack")
     else:
-        mesh.rotation_degrees = Vector3(180, 0, 0)
+        animation.set("current_animation", "BlackToWhite")
+    animation.stop()
 
+
+func reverse() -> void:
+    animation.play()
+
+
+func _on_animation_finished(anim_name: StringName) -> void:
+    if color == COLOR.WHITE:
+        color = COLOR.BLACK
+    else:
+        color = COLOR.WHITE
